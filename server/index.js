b752bef7f9e56d69b666f7cda4a0b20f330643b0
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-19 08:01:45 +0530

    feat: create first http route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 
-import express from 'express';
+import express, { response } from 'express';
 import { PORT } from './config.js';
 
 const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234.).send('Welcome to the Book Store App Server');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
