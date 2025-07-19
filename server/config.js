e33c47a69d80bfc59dbf3cae4071d8974fe984ec
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-19 08:49:45 +0530

    feat: install mongoose and connect mongo db

--- server/config.js
+++ server/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoURI = 'mongodb+srv://root:root123@book-store.cbeuhvl.mongodb.net/'; 
--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-
 import express, { response } from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoURI } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.get('/', (request, response) => {
     console.log(request);
-    return response.status(234.).send('Welcome to the Book Store App Server');
+    return response.status(205).send('Welcome to the Book Store App Server');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+    .connect(mongoURI)
+    .then(() => {
+        console.log('Connected to MongoDB successfully...');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
