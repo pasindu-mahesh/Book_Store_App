6db6f30db1dfd431228927909ad899bfab6fa0b2
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-22 23:07:26 +0530

    feat: compleate delete book

--- server/index.js
+++ server/index.js
@@ -97,7 +97,19 @@ app.put('/books/:id', async (request, response) => {
 });
 
 // delete a book
-app.delete();
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const result = await Book.findByIdAndDelete(id);
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' });
+        }
+        return response.status(200).send({ message: 'Book deleted successfully' });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
 
 mongoose
     .connect(mongoURI)
