e7a2c225df10961b7656c3017d993404b0f5033c
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-25 23:23:09 +0530

    feat:import routs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+
 
 function App() {
   return (
