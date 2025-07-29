cb45df7b71fe3a0c11c66b8a4f9e749baabacba1
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-29 12:43:45 +0530

    feat:show book in react

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+
 
 function Home() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+     setLoading(true);
+     axios
+      .get('http://localhost:5555/books')
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div>
-      
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Book List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-sky-800 text-4xl'/>
+        </Link>
+      </div>
+        {loading ? (
+          <spinner />
+        ) : (
+          <table className='w-full border-separate border-spacing-2'>
+            <thead>
+
+            </thead>
+            <tbody> 
+
+            </tbody>
+          </table>
+          )}
+
     </div>
   )
 }
