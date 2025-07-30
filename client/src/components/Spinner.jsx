fbad82ef39ff3bd279493038ae19767c91728c86
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-30 10:01:33 +0530

    update: spinner

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Spinner() {
   return (
-    <div className='animate-ping w-16 h-16 m-8 bg-sky-600'></div>
+    <div className="flex justify-center items-center h-40">
+      <div className="relative w-16 h-16">
+        <div className="absolute inset-0 rounded-full border-4 border-t-blue-500 border-b-transparent animate-spin"></div>
+        <div className="absolute inset-0 rounded-full border-4 border-l-blue-300 border-r-transparent animate-spin [animation-duration:1.5s]"></div>
+      </div>
+    </div>
   )
 }
 
