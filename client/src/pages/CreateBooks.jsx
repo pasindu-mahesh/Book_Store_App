820e2f826c0fcac50858d07ab505dfe1f8549d76
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-30 13:41:57 +0530

    feat: create handleSaveBook

--- client/src/pages/CreateBooks.jsx
+++ client/src/pages/CreateBooks.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-function CreateBooks() {
+const CreateBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSaveBook = () => {
+    
+  };
+
   return (
     <div>
       CreateBooks
