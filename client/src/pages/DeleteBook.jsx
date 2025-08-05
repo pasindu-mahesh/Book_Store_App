cc2038d288570494150754ba6310ccc3fdb377f2
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-08-05 23:47:24 +0530

    feat:compleat delete book

--- client/src/pages/DeleteBook.jsx
+++ client/src/pages/DeleteBook.jsx
@@ -29,6 +29,12 @@ const DeleteBook = () => {
       {loading ? <Spinner /> : ''}
       <div className='flex flex-col items-center border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <h3 className='text-2xl'>Are you sure to delete this book</h3>
+        <button
+          className='p-4 bg-red-600 text-white m-8 w-full'
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
 
       </div>
     </div>
