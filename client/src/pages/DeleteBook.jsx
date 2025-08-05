c81a08300d94db0039c9ee9f2a8b8bc28f9c89a1
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-08-05 23:42:37 +0530

    feat: confirmton msg

--- client/src/pages/DeleteBook.jsx
+++ client/src/pages/DeleteBook.jsx
@@ -4,10 +4,33 @@ import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
-function DeleteBook() {
+const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error occurred while deleting the book');
+        console.log(error);
+      });
+  };
   return (
-    <div>
-      delete book
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <h3 className='text-2xl'>Are you sure to delete this book</h3>
+
+      </div>
     </div>
   )
 }
