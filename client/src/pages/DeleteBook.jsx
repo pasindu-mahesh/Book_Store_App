5951fbb822ca6d25afe0f1f3829733f232ffa7f2
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-08-05 23:30:11 +0530

    feat: create delete Book

--- client/src/pages/DeleteBook.jsx
+++ client/src/pages/DeleteBook.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function DeleteBook() {
   return (
