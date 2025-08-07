fdfb01bb4b2eaef2a726129798815dba293d1bd3
pasindu-mahesh <99pasindumahesh@gmail.com>
2025-08-07 15:04:45 +0530

    create home component

---
+++ client/src/components/home/BooksCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BooksCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BooksCard
---
+++ client/src/components/home/BooksTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BooksTable() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BooksTable
